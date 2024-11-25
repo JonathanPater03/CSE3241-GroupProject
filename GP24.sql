68eb1c39d56571e965b398b9f7832d1a43cd4df4
rscharff <122933900+rscharff23@users.noreply.github.com>
2024-11-25 01:38:41 -0500

    primary key

--- GP24.sql
+++ GP24.sql
@@ -3,6 +3,7 @@ create schema gp24;
 use gp24;
 
 Create table if not exists VALS (
-	  Date date not null,
+    Date date not null,
     Stock varchar(5) not null,
-    Price decimal(10,2) not null) engine=innodb;
+    Price decimal(10,2) not null,
+    primary key (Date, Stock)) engine=innodb;
